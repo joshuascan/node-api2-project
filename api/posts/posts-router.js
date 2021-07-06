7948ed33fe604f85f4ffa82ba269d0adfb65ba24
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-06 12:28:58 -0700

    Creates scaffolding for posts-router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,34 @@ const Post = require("./posts-model");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.status(200).json(req.method + "posts");
+  console.log(req.method);
+});
+
+router.get("/:id", (req, res) => {
+  res.status(200).json(req.method + "post");
+  console.log(req.method);
+});
+
+router.post("/", (req, res) => {
+  res.status(200).json(req.method + "new post");
+  console.log(req.method);
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json(req.method + "post");
+  console.log(req.method);
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json(req.method + "post");
+  console.log(req.method);
+});
+
+router.get("/:id/comments", (req, res) => {
+  res.status(200).json(req.method + "comments");
+  console.log(req.method);
+});
+
 module.exports = router;
