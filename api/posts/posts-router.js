6bf8542265a97b6eb2b95f43fbc89e08a1400a41
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-06 12:43:29 -0700

    Creates get posts and get post handlers

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,13 +4,33 @@ const Post = require("./posts-model");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).json(req.method + "posts");
-  console.log(req.method);
+  Post.find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: "The posts information could not be retrieved" });
+    });
 });
 
 router.get("/:id", (req, res) => {
-  res.status(200).json(req.method + "post");
-  console.log(req.method);
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved" });
+    });
 });
 
 router.post("/", (req, res) => {
